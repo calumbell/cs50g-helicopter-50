d8d45f5c514924b10c431a6df0e2990008767269
Calum Bell <calumabell@googlemail.com>
2020-08-12 14:55:52 +0100

    refactored obs. col. code as Obstacle component  - deleted unused components

---
+++ Assets/Resources/Scripts/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public bool destroyObstacleOnCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // trigger helicopter to explode when it collides with this
+        other.transform.parent.gameObject.GetComponent<HeliController>().Explode();
+
+        // if our obstacle is tagged to be destroyed on collision, destroy it
+        if (destroyObstacleOnCollision)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
