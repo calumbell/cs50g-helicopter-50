9df1f43873ed4967c0bb9ef43dfc4a3925944d4a
Calum Bell <calumabell@googlemail.com>
2020-09-07 18:02:07 +0100

    refactored collectable script to make gem spawner more visible to grader

---
+++ Assets/Resources/Scripts/CoinSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpawner : MonoBehaviour {
+
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnCoins());
+	}
+
+
+	IEnumerator SpawnCoins() {
+		while (true) {
+
+			// number of coins we could spawn vertically
+			int coinsThisRow = Random.Range(1, 4);
+
+			// instantiate all coins in this row separated by some random amount of space
+			for (int i = 0; i < coinsThisRow; i++) {
+				Instantiate(prefab, new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			}
+
+
+
+			// pause 1-5 seconds until the next coin spawns
+			yield return new WaitForSeconds(Random.Range(1, 5));
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGem());
+    }
+
+    IEnumerator SpawnGem()
+    {
+        while (true)
+        {
+            // pause for 3-15 seconds
+            yield return new WaitForSeconds(Random.Range(3, 15));
+
+            // instantiate gem prefab
+            Instantiate(prefab, new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+        }
+    }
+}
